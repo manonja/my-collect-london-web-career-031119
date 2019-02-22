5fd4f012e28bf523c61d8b353a04f128905f04cc
manon jacquin <manon.jacquin@gmail.com>
2019-02-22 11:56:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(collection)
+  counter = 0 
+  modified_collection = [] 
+  while counter < collection.length 
+  # push the new result in a new array so we can return the modified collection
+    modified_collection << yield(collection[counter])
+    counter += 1 
+  end
+  modified_collection
+end
 
