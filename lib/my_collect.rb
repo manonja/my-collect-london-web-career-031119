1de38876f9565e4c531c48082477c8a3862a8faa
manon jacquin <manon.jacquin@gmail.com>
2019-02-22 11:56:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
-def my_collect(array)
+def my_collect(collection)
   counter = 0 
-  newArray = [] 
+  modified_collection = [] 
   while counter < array.length 
-  # push the new result in a new array
-    newArray << yield(array[counter])
+  # push the new result in a new array so we can return the modified collection
+    modified_collection << yield(collection[counter])
     counter += 1 
   end
-  newArray
+  modified_collection
 end
 
