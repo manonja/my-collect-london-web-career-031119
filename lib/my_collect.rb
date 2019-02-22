a8320c7d3f5bd48db99fb91c664809a6a3f38ec5
manon jacquin <manon.jacquin@gmail.com>
2019-02-22 11:54:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,11 @@
 def my_collect(array)
-  #starts counter
   counter = 0 
-  
+  newArray = [] 
+  while counter < array.length 
+  # push the new result in a new array
+    newArray << yield(array[counter])
+    counter += 1 
+  end
+  newArray
 end
 
